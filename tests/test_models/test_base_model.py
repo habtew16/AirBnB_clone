6642d5a33638250d92403a889d9c92bf70fca839
Habtamu <habtamuwolde96@gmail.com>
2023-11-11 01:56:41 -0800

    added doc for test cases

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -35,23 +35,30 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(p.total_errors, 0, "fix pep8")
 
     def test_check_functions(self):
+        """function to check if class has
+        doc file"""
         self.assertIsNotNone(BaseModel.__doc__)
         self.assertIsNotNone(BaseModel.save.__doc__)
         self.assertIsNotNone(BaseModel.to_dict.__doc__)
 
     def test_attribute_basemodel(self):
+        """function to check if attributes exist
+        in basemodel"""
         self.assertTrue(hasattr(BaseModel, "__init__"))
         self.assertTrue(hasattr(BaseModel, "save"))
         self.assertTrue(hasattr(BaseModel, "to_dict"))
 
     def test_init(self):
+        """check the existance of init"""
         self.assertTrue(isinstance(self.testBase, BaseModel))
 
     def test_save(self):
+        """check save"""
         self.testBase.save()
         self.assertNotEqual(self.testBase.created_at, self.testBase.updated_at)
 
     def test_to_dict(self):
+        """checkes to dict function of basemodel"""
         obj = self.testBase.to_dict()
         self.assertEqual(self.testBase.__class__.__name__, 'BaseModel')
         self.assertIsInstance(obj['created_at'], str)
