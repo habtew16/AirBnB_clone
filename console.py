bef88fa40fecbd4e8f8f1d0433797f9bb5f5f02c
Habtamu <habtamuwolde96@gmail.com>
2023-11-11 03:00:58 -0800

    added docs

--- console.py
+++ console.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+"""entry class for
+airbnb console cmd
+"""
 import cmd
 from models.base_model import BaseModel
 from models import storage
 from shlex import split
+import shlex
 
 
 class HBNBCommand(cmd.Cmd):
@@ -10,16 +14,111 @@ class HBNBCommand(cmd.Cmd):
     classes = {"BaseModel"}
 
     def do_quit(self, arg):
-        """Quit command to exit the program
-        """
         return True
 
     do_EOF = do_quit
 
     def emptyline(self):
-        """empty line nothing"""
         pass
 
+    def do_create(self, arg):
+        if not len(arg):
+            print("** class name missing **")
+            return
+        if arg not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+        newObject = eval(arg)()
+        print(newObject.id)
+        newObject.save()
 
-if __name__ == '__main__':
+    def do_show(self, arg):
+        """shows an object"""
+        if not len(arg):
+            print("** class name missing **")
+            return
+        strings = split(arg)
+        if strings[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+        if len(strings) == 1:
+            print("** instance id missing **")
+            return
+        keyValue = strings[0] + '.' + strings[1]
+        if keyValue not in storage.all().keys():
+            print("** no instance found **")
+        else:
+            print(storage.all()[keyValue])
+
+    def do_destroy(self, arg):
+        """deletes an object"""
+        if not len(arg):
+            print("** class name missing **")
+            return
+        strings = split(arg)
+        if strings[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+        if len(strings) == 1:
+            print("** instance id missing **")
+            return
+        keyValue = strings[0] + '.' + strings[1]
+        if keyValue not in storage.all().keys():
+            print("** no instance found **")
+            return
+        del storage.all()[keyValue]
+        storage.save()
+
+    def do_all(self, arg):
+        """prints all"""
+        if not len(arg):
+            print([obj for obj in storage.all().values()])
+            return
+        strings = split(arg)
+        if strings[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+        print([obj for obj in storage.all().values()
+               if strings[0] == type(obj).__name__])
+
+    def do_update(self, arg):
+        """updates an object"""
+        if not len(arg):
+            print("** class name missing **")
+            return
+        strings = split(arg)
+        for string in strings:
+            if string.startswith('"') and string.endswith('"'):
+                string = string[1:-1]
+        if strings[0] not in HBNBCommand.classes:
+            print("** class doesn't exist **")
+            return
+        if len(strings) == 1:
+            print("** instance id missing **")
+            return
+        keyValue = strings[0] + '.' + strings[1]
+        if keyValue not in storage.all().keys():
+            print("** no instance found **")
+            return
+        if len(strings) == 2:
+            print("** attribute name missing **")
+            return
+        if len(strings) == 3:
+            print("** value missing **")
+            return
+        try:
+            setattr(storage.all()[keyValue], strings[2], eval(strings[3]))
+        except Exception:
+            setattr(storage.all()[keyValue], strings[2], strings[3])
+
+    def stripper(self, st):
+        """strips that line"""
+        newstring = st[st.find("(")+1:st.rfind(")")]
+        newstring = shlex.shlex(newstring, posix=True)
+        newstring.whitespace += ','
+        newstring.whitespace_split = True
+        return list(newstring)
+
+
+if __name__ == "__main__":
     HBNBCommand().cmdloop()
