f160068126bd237d039e4c2c704b9dd86a3f8980
Habtamu <habtamuwolde96@gmail.com>
2023-11-12 01:44:49 -0800

    added more unittest ffor basemodel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,69 +1,86 @@
 #!/usr/bin/python3
-"""unittests the basemodels"""
 import unittest
 import os
 import pep8
+from datetime import datetime
 from models.base_model import BaseModel
+import models
 
 
 class TestBaseModel(unittest.TestCase):
-    """all the tests for basemodel"""
 
-    @classmethod
-    def setUp(self):
-        """set up"""
-        self.testBase = BaseModel()
-        self.testBase.x = "x"
-        self.testBase.y = 100
-
-    @classmethod
-    def tearDown(self):
-        """tear down
+    def test_pep8_base_model(self):
         """
-        del self.testBase
-        try:
-            os.remove("file.json")
-        except Exception:
-            pass
-
-    def test_pep8_basemodel(self):
-        """
-        tests pep8
+        Test that models/base_model.py conforms to PEP8.
         """
         style = pep8.StyleGuide(quiet=True)
-        p = style.check_files(['models/base_model.py'])
-        self.assertEqual(p.total_errors, 0, "fix pep8")
+        result = style.check_files(['models/base_model.py'])
+        self.assertEqual(result.total_errors, 0, "Fix PEP8 style issues")
 
-    def test_check_functions(self):
-        """function to check if class has
-        doc file"""
-        self.assertIsNotNone(BaseModel.__doc__)
-        self.assertIsNotNone(BaseModel.save.__doc__)
-        self.assertIsNotNone(BaseModel.to_dict.__doc__)
+    def test_base_model_instance(self):
+        """
+        Test the instantiation of the BaseModel class.
+        """
+        test_base = BaseModel()
+        self.assertIsInstance(test_base, BaseModel)
 
-    def test_attribute_basemodel(self):
-        """function to check if attributes exist
-        in basemodel"""
-        self.assertTrue(hasattr(BaseModel, "__init__"))
-        self.assertTrue(hasattr(BaseModel, "save"))
-        self.assertTrue(hasattr(BaseModel, "to_dict"))
+    def test_base_model_id_type(self):
+        """
+        Test the type of the id attribute in BaseModel.
+        """
+        test_base = BaseModel()
+        self.assertIsInstance(test_base.id, str)
 
-    def test_init(self):
-        """check the existance of init"""
-        self.assertTrue(isinstance(self.testBase, BaseModel))
+    def test_base_model_created_at_type(self):
+        """
+        Test the type of the created_at attribute in BaseModel.
+        """
+        test_base = BaseModel()
+        self.assertIsInstance(test_base.created_at, datetime)
 
-    def test_save(self):
-        """check save"""
-        self.testBase.save()
-        self.assertNotEqual(self.testBase.created_at, self.testBase.updated_at)
+    def test_base_model_updated_at_type(self):
+        """
+        Test the type of the updated_at attribute in BaseModel.
+        """
+        test_base = BaseModel()
+        self.assertIsInstance(test_base.updated_at, datetime)
 
-    def test_to_dict(self):
-        """checkes to dict function of basemodel"""
-        obj = self.testBase.to_dict()
-        self.assertEqual(self.testBase.__class__.__name__, 'BaseModel')
-        self.assertIsInstance(obj['created_at'], str)
-        self.assertIsInstance(obj['updated_at'], str)
+    def test_base_model_save(self):
+        """
+        Test the save method in BaseModel.
+        """
+        test_base = BaseModel()
+        prev_updated_at = test_base.updated_at
+        test_base.save()
+        self.assertNotEqual(prev_updated_at, test_base.updated_at)
+
+    def test_base_model_to_dict(self):
+        """
+        Test the to_dict method in BaseModel.
+        """
+        test_base = BaseModel()
+        obj_dict = test_base.to_dict()
+        self.assertEqual(obj_dict['__class__'], 'BaseModel')
+        self.assertIsInstance(obj_dict['created_at'], str)
+        self.assertIsInstance(obj_dict['updated_at'], str)
+
+    def test_base_model_str(self):
+        """
+        Test the __str__ method in BaseModel.
+        """
+        test_base = BaseModel()
+        expected_str = "[BaseModel] ({}) {}".format(
+            test_base.id, test_base.__dict__)
+        self.assertEqual(str(test_base), expected_str)
+
+    def test_base_model_repr(self):
+        """
+        Test the __repr__ method in BaseModel.
+        """
+        test_base = BaseModel()
+        expected_repr = str(test_base)
+        self.assertEqual(repr(test_base), expected_repr)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
